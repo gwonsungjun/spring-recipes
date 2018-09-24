a255be15735cd5d3307e6793545ee8be9fae48be
Sungjun <sungjunpizz@gmail.com>
2018-09-24 23:14:57 +0900

    Update recipes 3-7

--- src/main/java/ch03/com/springrecipes/court/config/ViewResolverConfiguration.java
+++ src/main/java/ch03/com/springrecipes/court/config/ViewResolverConfiguration.java
@@ -4,11 +4,18 @@ import org.springframework.context.ResourceLoaderAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.MediaType;
+import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.ResourceBundleViewResolver;
 import org.springframework.web.servlet.view.XmlViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /*public class ViewResolverConfiguration implements WebMvcConfigurer, ResourceLoaderAware {
@@ -34,12 +41,30 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 }*/
 @Configuration
-public class ViewResolverConfiguration {
+public class ViewResolverConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer){
+        Map<String, MediaType> mediatypes = new HashMap<>();
+        mediatypes.put("html", MediaType.TEXT_HTML);
+        mediatypes.put("pdf", MediaType.valueOf("application/json"));
+        mediatypes.put("xls", MediaType.valueOf("application/vnd.ms-excel"));
+        mediatypes.put("xml", MediaType.APPLICATION_XML);
+        mediatypes.put("json", MediaType.APPLICATION_JSON);
+        configurer.mediaTypes(mediatypes);
+    }
+
+    @Bean
+    public ContentNegotiatingViewResolver contentNegotiatingViewResolver(ContentNegotiationManager contentNegotiationManager){
+        ContentNegotiatingViewResolver viewResolver = new ContentNegotiatingViewResolver();
+        viewResolver.setContentNegotiationManager(contentNegotiationManager);
+        return viewResolver;
+    }
 
     @Bean
     public ResourceBundleViewResolver viewResolver(){
         ResourceBundleViewResolver viewResolver = new ResourceBundleViewResolver();
-        viewResolver.setOrder(0);
+       // viewResolver.setOrder(0);
         viewResolver.setBasename("court-views");
         return viewResolver;
     }
@@ -51,7 +76,7 @@ public class ViewResolverConfiguration {
     public InternalResourceViewResolver internalResourceViewResolver() {
 
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setOrder(1);
+       // viewResolver.setOrder(1);
         viewResolver.setPrefix("/WEB-INF/jsp/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
