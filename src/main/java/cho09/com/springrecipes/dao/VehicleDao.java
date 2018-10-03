4e6c8b1936650e5fade676efc190a7be29afa5af
Sungjun <sungjunpizz@gmail.com>
2018-10-03 14:12:53 +0900

    Update recipes 9-0

---
+++ src/main/java/cho09/com/springrecipes/dao/VehicleDao.java
@@ -0,0 +1,19 @@
+package cho09.com.springrecipes.dao;
+
+import cho09.com.springrecipes.domin.Vehicle;
+
+import java.util.List;
+
+public interface VehicleDao {
+    void insert(Vehicle vehicle);
+
+    void insert(Iterable<Vehicle> vehicles);
+
+    void update(Vehicle vehicle);
+
+    void delete(Vehicle vehicle);
+
+    Vehicle findByVehicleNo(String vehicleNo);
+
+    List<Vehicle> findByAll();
+}
