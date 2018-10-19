08eedcf78339e8ed11c294506a222ac7d58cf3f1
Sungjun <sungjunpizz@gmail.com>
2018-10-19 23:37:38 +0900

    Update recipes 5-1 DeferredResult

--- src/main/java/ch05/com/springrecipes/court/controller/ReservationQueryController.java
+++ src/main/java/ch05/com/springrecipes/court/controller/ReservationQueryController.java
@@ -3,12 +3,15 @@ package ch05.com.springrecipes.court.controller;
 import ch05.com.springrecipes.court.service.ReservationService;
 import ch05.com.springrecipes.court.Delayer;
 import ch05.com.springrecipes.court.domain.Reservation;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -18,9 +21,11 @@ import java.util.concurrent.Callable;
 public class ReservationQueryController {
 
     private final ReservationService reservationService;
+    private final TaskExecutor taskExecutor;
 
-    public ReservationQueryController(ReservationService reservationService) {
+    public ReservationQueryController(ReservationService reservationService, AsyncTaskExecutor taskExecutor) {
         this.reservationService = reservationService;
+        this.taskExecutor =taskExecutor;
     }
 
     @GetMapping
@@ -37,5 +42,21 @@ public class ReservationQueryController {
             model.addAttribute("reservations", reservations);
             return "reservationQuery";
         };
+    }
+
+    @PostMapping
+    public DeferredResult<String> submitForm2(@RequestParam("counrtName") String courtName, Model model){
+        final DeferredResult<String> result = new DeferredResult<>();
+
+        taskExecutor.execute(() -> {
+            List<Reservation> reservations = java.util.Collections.emptyList();
+            if(courtName != null){
+                Delayer.randomDelay();
+                reservations = reservationService.query(courtName);
+            }
+            model.addAttribute("reservations", reservations);
+            result.setResult("reservationQuery");
+        });
+        return result;
     }
 }
