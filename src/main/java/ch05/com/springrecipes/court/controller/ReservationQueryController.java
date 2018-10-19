ec37305048acd0ad10274d7f0d4d44941c84709f
Sungjun <sungjunpizz@gmail.com>
2018-10-20 00:11:16 +0900

    Update recipes 5-1 finish

--- src/main/java/ch05/com/springrecipes/court/controller/ReservationQueryController.java
+++ src/main/java/ch05/com/springrecipes/court/controller/ReservationQueryController.java
@@ -3,10 +3,12 @@ package ch05.com.springrecipes.court.controller;
 import ch05.com.springrecipes.court.service.ReservationService;
 import ch05.com.springrecipes.court.Delayer;
 import ch05.com.springrecipes.court.domain.Reservation;
+import org.springframework.core.task.AsyncListenableTaskExecutor;
 import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,17 +17,24 @@ import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
 
 @Controller
 @RequestMapping("/reservationQuery")
 public class ReservationQueryController {
 
     private final ReservationService reservationService;
-    private final TaskExecutor taskExecutor;
+    private final AsyncListenableTaskExecutor taskExecutor;
+    //private final TaskExecutor taskExecutor;
 
-    public ReservationQueryController(ReservationService reservationService, AsyncTaskExecutor taskExecutor) {
+/*    public ReservationQueryController(ReservationService reservationService, AsyncTaskExecutor taskExecutor) {
         this.reservationService = reservationService;
         this.taskExecutor =taskExecutor;
+    }*/
+
+    public ReservationQueryController(ReservationService reservationService, AsyncListenableTaskExecutor taskExecutor) {
+        this.reservationService = reservationService;
+        this.taskExecutor = taskExecutor;
     }
 
     @GetMapping
@@ -59,4 +68,33 @@ public class ReservationQueryController {
         });
         return result;
     }
+
+    @PostMapping
+    public CompletableFuture<String> submitForm3(@RequestParam("courtName") String courtName, Model model) {
+
+        return CompletableFuture.supplyAsync(() -> {
+            List<Reservation> reservations = java.util.Collections.emptyList();
+            if(courtName != null){
+                Delayer.randomDelay();
+                reservations = reservationService.query(courtName);
+            }
+            model.addAttribute("reservations", reservations);
+            return "reservationQuery";
+        }, taskExecutor);
+    }
+
+    @PostMapping
+    public ListenableFuture<String> submitForm4(@RequestParam("courtName") String courtName, Model model) {
+
+        return taskExecutor.submitListenable(() -> {
+            List<Reservation> reservations = java.util.Collections.emptyList();
+            if(courtName != null){
+                Delayer.randomDelay();
+                reservations = reservationService.query(courtName);
+            }
+            model.addAttribute("reservations", reservations);
+            return "reservationQuery";
+        });
+    }
+
 }
